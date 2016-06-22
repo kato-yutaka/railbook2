59c27e5272754f3e55dfe3a87c07f31f5484697a
kato-yutaka <kato-yutaka@outlook.jp>
2016-06-22 11:27:27 +0900

    12章変更点７終了後

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -63,6 +63,7 @@ class BooksController < ApplicationController
 
   def search
     @books = Book.where(title: params["search"]["title"])
+    @kensaku = params["search"]["title"]
     render :index
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415032212) do
+ActiveRecord::Schema.define(version: 20160620044359) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20160415032212) do
     t.boolean  "cd"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "publishes", force: :cascade do |t|
+    t.string   "publishname"
+    t.string   "Address"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
